f6bd903fa41fc3be3123a86cc7dac52ca61aa9b7
JoseALermaIII <jose@JoseALerma.com>
2019-04-28 22:32:12 -0500

    Add docstring and __version__

---
+++ givecookiebotslack/__init__.py
@@ -0,0 +1,9 @@
+"""givecookiebot-slack: Slack bot that adds a cookie emoji (🍪) to a user's
+status for 3 hours when that user is 'given' a cookie in chat.
+
+GPLv3 License
+
+Austin Developer Community
+"""
+
+__version__ = '0.2.0'
