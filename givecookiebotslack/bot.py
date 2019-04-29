4fb32923f90cb91beb0660b9c08ca7371e445a75
JoseALermaIII <jose@JoseALerma.com>
2019-04-28 21:06:29 -0500

    Incorporate Slack's example bot

--- givecookiebotslack/bot.py
+++ givecookiebotslack/bot.py
@@ -6,10 +6,45 @@ Defines `Event API`_ events that the bot will respond to.
 """
 
 import os
-
 from slackeventsapi import SlackEventAdapter
 from slackclient import SlackClient
-from flask import Flask
+
+# Our app's Slack Event Adapter for receiving actions via the Events API
+SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
+slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/slack/events")  #pylint: disable=invalid-name
+
+# Create a SlackClient for your bot to use for Web API requests
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+slack_client = SlackClient(SLACK_BOT_TOKEN)  # pylint: disable=invalid-name
+
+@slack_events_adapter.on("message")
+def handle_message(event_data):
+    """Example responder to greetings
+    """
+    message = event_data["event"]
+    # If the incoming message contains "hi", then respond with a "Hello" message
+    if message.get("subtype") is None and "hi" in message.get('text'):
+        channel = message["channel"]
+        message = "Hello <@%s>! :tada:" % message["user"]
+        slack_client.api_call("chat.postMessage", channel=channel, text=message)
 
 
-app = Flask(__name__)  # pylint: disable=invalid-name
+@slack_events_adapter.on("reaction_added")
+def reaction_added(event_data):
+    """Example reaction emoji echo
+    """
+    event = event_data["event"]
+    emoji = event["reaction"]
+    channel = event["item"]["channel"]
+    text = ":%s:" % emoji
+    slack_client.api_call("chat.postMessage", channel=channel, text=text)
+
+@slack_events_adapter.on("error")
+def error_handler(err):
+    """Error events
+    """
+    print("ERROR: " + str(err))
+
+# Once we have our event listeners configured, we can start the
+# Flask server with the default `/events` endpoint on port 3000
+slack_events_adapter.start(port=3000)
