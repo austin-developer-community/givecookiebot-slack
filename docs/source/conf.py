230bf43262cc30922b1caa933cad69947ec9c9e0
JoseALermaIII <jose@JoseALerma.com>
2019-05-01 04:57:09 -0500

    Bump to v0.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Austin Developer Community'
 version = '0.2'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.1'
 
 
 # -- General configuration ---------------------------------------------------
